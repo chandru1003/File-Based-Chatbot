4aec0bf0e24fa83b8a90d56bd0dde8b404a83767
chandru <chandrukongari@gmail.com>
2023-11-20 21:04:31 +0530

    haystack added

--- chatbot.py
+++ chatbot.py
@@ -3,16 +3,25 @@ import pdfplumber
 from docx import Document
 import PySimpleGUI as sg
 from transformers import pipeline, T5Tokenizer
+from haystack import Pipeline
+from haystack.reader.farm import FARMReader
+from haystack.retriever.dense import DensePassageRetriever
+from haystack.document_store.memory import InMemoryDocumentStore
+
 
 class FileBasedChatbot:
     def __init__(self):
         self.folder_path = ""
         self.extracted_text = ""
+        self.document_store = InMemoryDocumentStore()
+        self.reader = FARMReader(model_name_or_path="distilbert-base-uncased-distilled-squad")
+        self.retriever = DensePassageRetriever(document_store=self.document_store,
+                                               query_embedding_model="facebook/dpr-question_encoder-single-nq-base",
+                                               passage_embedding_model="facebook/dpr-ctx_encoder-single-nq-base")
+        self.pipeline = Pipeline(reader=self.reader, retriever=self.retriever)
+
         self.create_window()
-        self.summarization_pipeline = pipeline("summarization", model="t5-base", tokenizer=T5Tokenizer.from_pretrained("t5-base", model_max_length=1024), framework="tf")
-        self.qa_pipeline = pipeline("question-answering", model="Falconsai/question_answering")  
-         
-        
+
     def extract_text_from_documents(self):
         extracted_texts = []
 
@@ -37,20 +46,26 @@ class FileBasedChatbot:
         with open("extracted_text.txt", "w", encoding="utf-8") as text_file:
             text_file.write(extracted_text)
 
-        return extracted_text  
+        return extracted_text
+
+    def index_documents(self, documents):
+        self.document_store.write_documents(documents)
 
     def summarize_text(self, combined_text):
-        summary = self.summarization_pipeline(combined_text, max_length=100, min_length=60, do_sample=False)
+        summarization_pipeline = pipeline("summarization", model="t5-base", tokenizer=T5Tokenizer.from_pretrained("t5-base", model_max_length=1024), framework="tf")
+        summary = summarization_pipeline(combined_text, max_length=100, min_length=60, do_sample=False)
         return summary[0]['summary_text']
 
     def process_user_input(self, user_input):
         if not self.extracted_text:
             return "Please select a folder with documents first."
 
-        answer = self.qa_pipeline(question=user_input, context=self.extracted_text)
+        documents = [{"text": self.extracted_text}]
+        self.index_documents(documents)
 
-        if answer['score'] > 0.5:
-            return f"Answer found: {answer['answer']}"
+        reader_results = self.pipeline.run(query=user_input, top_k_retriever=5)
+        if reader_results["answers"] and reader_results["answers"][0]["probability"] > 0.5:
+            return f"Answer found: {reader_results['answers'][0]['answer']}"
         else:
             matches = [sentence for sentence in self.extracted_text.split('.') if user_input.lower() in sentence.lower()]
             return "Answer found: " + self.summarize_text(' '.join(matches)) if matches else "Sorry, I couldn't find an answer related to your query."
@@ -65,7 +80,7 @@ class FileBasedChatbot:
         self.window = sg.Window('File-Based Chatbot', layout, finalize=True)
         self.window.set_icon('icon\icons8-chat-bot-64.ico')
 
-    def run(self):    
+    def run(self):
         while True:
             event, values = self.window.read()
             if event in (sg.WIN_CLOSED, 'Exit'):
@@ -75,7 +90,7 @@ class FileBasedChatbot:
                 new_folder_path = values['-FOLDER_PATH-']
 
                 if new_folder_path:
-                    self.folder_path = new_folder_path                    
+                    self.folder_path = new_folder_path
                     self.extracted_text = self.extract_text_from_documents()
 
                 chatbot_response = self.process_user_input(user_input)
@@ -84,5 +99,5 @@ class FileBasedChatbot:
                 self.window['-IN-'].update('')
 
 if __name__ == "__main__":
-    chatbot = FileBasedChatbot()     
+    chatbot = FileBasedChatbot()
     chatbot.run()
